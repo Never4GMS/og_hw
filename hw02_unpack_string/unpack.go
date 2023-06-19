064aad00dd0674589cad7b6ba93471330383c15c
Viktor <never4gms@gmail.com>
2023-06-19 23:55:32 +0300

    HW02 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,67 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
+
+const zero int = int(rune('0'))
+const escape rune = '\\'
+const none rune = 0
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	sb := strings.Builder{}
+
+	var symbol rune
+	escaping := false
+	for _, currentSymbol := range input {
+		switch {
+		case unicode.IsDigit(currentSymbol):
+			if escaping {
+				symbol = currentSymbol
+				escaping = false
+				continue
+			}
+
+			if symbol == none {
+				return "", ErrInvalidString
+			}
+
+			sb.WriteString(strings.Repeat(string(symbol), int(currentSymbol)-zero))
+			symbol = none
+
+		case currentSymbol == escape:
+			if escaping {
+				symbol = currentSymbol
+				escaping = false
+				continue
+			}
+
+			if symbol != none {
+				sb.WriteRune(symbol)
+				symbol = none
+			}
+
+			escaping = true
+
+		case unicode.IsLetter(currentSymbol):
+			if escaping {
+				return "", ErrInvalidString
+			}
+
+			if symbol != none {
+				sb.WriteRune(symbol)
+			}
+
+			symbol = currentSymbol
+		}
+	}
+
+	if symbol != none {
+		sb.WriteRune(symbol)
+	}
+
+	return sb.String(), nil
 }
