e37e7382fff1b489c5baceac95b9f364d36c06ce
Viktor <never4gms@gmail.com>
2023-06-21 17:42:51 +0300

    Simplify top.go

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -11,11 +11,6 @@ const (
 	hyphenString string = "-"
 )
 
-type wordStat struct {
-	word  string
-	count uint16
-}
-
 func Top10(text string) []string {
 	return top10(createRating(createWordMap(text)))
 }
@@ -41,36 +36,32 @@ func createWordMap(text string) map[string]uint16 {
 	return counters
 }
 
-func createRating(counters map[string]uint16) []wordStat {
-	rating := make([]wordStat, 0, len(counters))
-	for word, count := range counters {
-		rating = append(rating, wordStat{
-			word:  word,
-			count: count,
-		})
+func createRating(counters map[string]uint16) []string {
+	rating := make([]string, 0, len(counters))
+	for word := range counters {
+		rating = append(rating, word)
 	}
 	sort.Slice(rating, func(i, j int) bool {
-		if rating[i].count == rating[j].count {
-			return strings.Compare(rating[i].word, rating[j].word) < 0
+		left := rating[i]
+		leftCount := counters[left]
+		right := rating[j]
+		rightCount := counters[right]
+
+		if leftCount == rightCount {
+			return strings.Compare(left, right) < 0
 		}
 
-		return rating[i].count > rating[j].count
+		return leftCount > rightCount
 	})
 
 	return rating
 }
 
-func top10(rating []wordStat) []string {
+func top10(rating []string) []string {
 	limit := 10
 	if len(rating) < 10 {
 		limit = len(rating)
 	}
 
-	top := make([]string, 0, limit)
-
-	for i := 0; i < limit; i++ {
-		top = append(top, rating[i].word)
-	}
-
-	return top
+	return rating[:limit]
 }
