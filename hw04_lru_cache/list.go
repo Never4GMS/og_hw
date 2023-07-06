63c7e93f4ec572d4ea527f1288fce341c02ac3ee
Viktor <never4gms@gmail.com>
2023-07-07 00:17:07 +0300

    HW04 comleted

--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -10,15 +10,120 @@ type List interface {
 	MoveToFront(i *ListItem)
 }
 
+type list struct {
+	count int
+	front *ListItem
+	back  *ListItem
+}
+
+func (l *list) Len() int {
+	return l.count
+}
+
+func (l *list) Front() *ListItem {
+	return l.front
+}
+
+func (l *list) Back() *ListItem {
+	return l.back
+}
+
+func (l *list) PushBack(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+	}
+
+	item.addBetween(l.back, nil)
+	l.back = item
+	if l.front == nil {
+		l.front = item
+	}
+	l.count++
+
+	return item
+}
+
+func (l *list) PushFront(v interface{}) *ListItem {
+	item := &ListItem{
+		Value: v,
+	}
+
+	item.addBetween(nil, l.front)
+	l.front = item
+	if l.back == nil {
+		l.back = item
+	}
+	l.count++
+
+	return item
+}
+
+func (l *list) Remove(i *ListItem) {
+	if i == nil {
+		return
+	}
+
+	if i == l.front {
+		l.front = i.Next
+	}
+
+	if i == l.back {
+		l.back = i.Prev
+	}
+
+	i.removeSelf()
+	l.count--
+}
+
+func (l *list) MoveToFront(i *ListItem) {
+	if i == nil {
+		return
+	}
+
+	if i == l.front {
+		return
+	}
+
+	if i == l.back {
+		l.back = i.Prev
+	}
+
+	i.removeSelf()
+	i.addBetween(nil, l.front)
+	l.front = i
+}
+
 type ListItem struct {
 	Value interface{}
 	Next  *ListItem
 	Prev  *ListItem
 }
 
-type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+func (i *ListItem) removeSelf() {
+	if i.Prev != nil {
+		i.Prev.Next = nil
+	}
+
+	if i.Next != nil {
+		i.Next.Prev = nil
+	}
+
+	if i.Prev != nil && i.Next != nil {
+		i.Prev.Next = i.Next
+		i.Next.Prev = i.Prev
+	}
+}
+
+func (item *ListItem) addBetween(prev, next *ListItem) {
+	if prev != nil {
+		prev.Next = item
+		item.Prev = prev
+	}
+
+	if next != nil {
+		next.Prev = item
+		item.Next = next
+	}
 }
 
 func NewList() List {
